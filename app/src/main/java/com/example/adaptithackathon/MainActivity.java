ff968ae6fcef817449a2f5c1228237d7a4e5514f
dean-south <solomon.mdean@gmail.com>
2023-07-29 13:12:58 +0200

    adding reg

--- app/src/main/java/com/example/adaptithackathon/MainActivity.java
+++ app/src/main/java/com/example/adaptithackathon/MainActivity.java
@@ -2,16 +2,53 @@ package com.example.adaptithackathon;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("Fuck me!");
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewRegister = findViewById(R.id.textViewRegister);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                // Sample login logic (Replace this with your actual login logic)
+                if (username.equals("user") && password.equals("password")) {
+                    showToast("Login successful!");
+                } else {
+                    showToast("Login failed. Invalid credentials.");
+                }
+            }
+        });
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Registration feature is not yet implemented.");
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
