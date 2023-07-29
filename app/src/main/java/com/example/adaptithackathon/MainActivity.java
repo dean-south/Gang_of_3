c002f20b709e2fb900a0bf2123ba3fcdcd9081bf
dean-south <solomon.mdean@gmail.com>
2023-07-29 11:04:15 +0200

    Template working

---
+++ app/src/main/java/com/example/adaptithackathon/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.adaptithackathon;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Hello, World!");
+    }
+}
