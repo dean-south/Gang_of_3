9ad7e9f940921bc121e3e364f626df088a267585
dean-south <solomon.mdean@gmail.com>
2023-07-29 12:46:33 +0200

    finally working android studio

--- app/src/main/java/com/example/adaptithackathon/MainActivity.java
+++ app/src/main/java/com/example/adaptithackathon/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textView);
-        textView.setText("Hello, World!");
+        textView.setText("Fuck me!");
     }
 }
